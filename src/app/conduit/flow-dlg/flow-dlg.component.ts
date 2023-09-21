51fffd051996b6658444e77efe77fe7f94055707
bluntsoftware <alexm@bluntsoftware.com>
2023-09-21 02:42:30 -0400

    commit files before angular update

---
+++ src/app/conduit/flow-dlg/flow-dlg.component.ts
@@ -0,0 +1,31 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {Flow} from "../../services/app-model";
+import {FormBuilder, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-flow-dlg',
+  templateUrl: './flow-dlg.component.html',
+  styleUrls: ['./flow-dlg.component.scss']
+})
+export class FlowDlgComponent implements OnInit {
+
+  form:FormGroup;
+  constructor(private dialog:MatDialogRef<FlowDlgComponent>,private fb: FormBuilder,@Inject(MAT_DIALOG_DATA) public data:Flow) {
+    this.form = this.createForm();
+  }
+  ngOnInit(): void {
+  }
+  public createForm() :FormGroup {
+    return this.fb.group({
+      "name": [this.data?.name],
+    });
+  }
+  cancel() {
+    this.dialog.close();
+  }
+  save() {
+
+    this.dialog.close(this.form.getRawValue());
+  }
+}
