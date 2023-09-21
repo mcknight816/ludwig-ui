c52851f1262b441b202203532da8756e82eae09a
bluntsoftware <alexm@bluntsoftware.com>
2023-09-21 02:38:19 -0400

    commit files before angular update

---
+++ src/app/application/app-edit/app-edit.component.ts
@@ -0,0 +1,16 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+
+@Component({
+  selector: 'app-app-edit',
+  templateUrl: './app-edit.component.html',
+  styleUrls: ['./app-edit.component.scss']
+})
+export class AppEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
