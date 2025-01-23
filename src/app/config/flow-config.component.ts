27b0cdd05010252a11044abcdc69dc63e641980e
bluntsoftware <alexm@bluntsoftware.com>
2025-01-23 07:37:26 -0500

    fix issue not loading config

--- src/app/config/flow-config.component.ts
+++ src/app/config/flow-config.component.ts
@@ -1,36 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {FormBuilder} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {PageEvent} from "@angular/material/paginator";
 import {FlowConfigService} from "./flow-config.service";
 import {ActivityConfigService} from "./activity-config.service";
 import {FlowConfig} from "./flow-config.model";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {Schema} from "../util/json-editor/json-schema-model";
+import {JsonEditorComponent} from "../util/json-editor/json-editor.component";
 
 @Component({
   selector: 'app-config',
   templateUrl: './flow-config.component.html',
   styleUrls: ['./flow-config.component.scss']
 })
-export class FlowConfigComponent {
+export class FlowConfigComponent implements OnInit {
+  @ViewChild(JsonEditorComponent) jsonEditor:JsonEditorComponent | undefined;
   private _currentSearchValue:string ='';
   private _currentPage: number = 1;
   private _pageSize: number = 20;
   public _dataLength: number = 0;
-
+  isLargeScreen = false;
+  schema: Schema | undefined;
   dataSource:Array<FlowConfig>  = [];
-
+  selectedItem: FlowConfig | undefined;
   tableColumns = [
     'name',
-    'configClass',
     'action'
   ];
   configTypes: Array<any> = [];
-
-  constructor(private fb: FormBuilder, private service:FlowConfigService, private activityConfigService: ActivityConfigService, private router: Router, private route: ActivatedRoute) {
+//  'configClass',
+  constructor(private breakpointObserver: BreakpointObserver, private fb: FormBuilder, private service:FlowConfigService, private activityConfigService: ActivityConfigService, protected router: Router, private route: ActivatedRoute) {
 
   }
 
   ngOnInit(): void {
+    this.breakpointObserver.observe([Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge]).subscribe(result => {
+      this.isLargeScreen = result.matches; // True if the screen is medium or larger
+    });
+
     this.activityConfigService.list().subscribe(c=>{
       this.configTypes = c;
     })
@@ -79,5 +87,25 @@ export class FlowConfigComponent {
     this._currentPage = $event.pageIndex + 1;
     this._pageSize = $event.pageSize;
     return this.search();
+  }
+
+  onRowClick(item: FlowConfig) {
+    if (this.isLargeScreen && item.configClass) {
+      this.selectedItem = item;
+      this.activityConfigService.getById(item?.configClass).subscribe(c =>{
+        this.schema = c.schema;
+      });
+    } else {
+    //  this.router.navigate(['/config/edit/' + item.configClass, item.id]); // Navigate to edit page for smaller screens
+    }
+  }
+
+  save(item: FlowConfig){
+    if(item){
+      item.config = this.jsonEditor?.form.getRawValue();
+      this.service.save(item).subscribe((c)=>{
+        item = c;
+      });
+    }
   }
 }
